f3f39f54b38cb74663439c387c2f6c3eaa6205b0
Venojan Shanmugarajah <VenojanShanmugarajah@gmail.com>
2025-02-13 21:41:08 +0530

    book cluub post

---
+++ Backend/storysoundhub-app/database/migrations/2025_02_13_155430_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('post_id');
+            $table->foreignid('membership_id')->constrained('book_club_members', 'membership_id')->onDelete('cascade');
+            $table->foreignuuid('club_id')->nullable()->constrained('book_clubs', 'club_id')->onDelete('cascade');
+            $table->text('content');
+            $table->string('media')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
