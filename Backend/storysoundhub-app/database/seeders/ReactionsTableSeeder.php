f65d08f9bb7070fc2e5079dba6abe47e0b96b37d
Venojan Shanmugarajah <VenojanShanmugarajah@gmail.com>
2025-02-19 22:58:15 +0530

    seeder

---
+++ Backend/storysoundhub-app/database/seeders/ReactionsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReactionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
