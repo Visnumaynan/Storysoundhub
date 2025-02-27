8d9ee6f6a64a42debb6182661d62cedd9630f68a
Dharnishan <dharnishan@gmail.com>
2025-02-27 12:58:10 +0530

    add some models

---
+++ Backend/storysoundhub-app/app/Models/book_whishlist.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class book_whishlist extends Model
+{
+    use HasFactory;
+
+    protected $table = 'book_wishlist';
+    protected $primaryKey = 'wishlist_id';
+    protected $fillable = ['user_id', 'book_id', 'picture', 'wishlist_date'];
+
+    protected $casts = [
+        'wishlist_date' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(users::class, 'user_id');
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(book::class, 'book_id');
+    }
+}
