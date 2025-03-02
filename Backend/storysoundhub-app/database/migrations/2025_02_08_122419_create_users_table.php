8a9267142c526c7dff63124258352426ef7883f8
Venojan Shanmugarajah <VenojanShanmugarajah@gmail.com>
2025-03-02 12:39:06 +0530

    correction

--- Backend/storysoundhub-app/database/migrations/2025_02_08_122419_create_users_table.php
+++ Backend/storysoundhub-app/database/migrations/2025_02_08_122419_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password_hash');
             $table->string('profile_picture')->nullable();
-            $table->integer('phone');
+            $table->string('phone');
             $table->string('location')->nullable();
             $table->timestamp('date_joined')->useCurrent();
             $table->timestamps();
