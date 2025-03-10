d9ca599dd77d802c58dde83414ea356d7f13dec0
Venojan Shanmugarajah <VenojanShanmugarajah@gmail.com>
2025-03-10 15:47:13 +0530

    correction 1- 3/10

--- Backend/storysound-app/app/Models/book_club_members.php
+++ Backend/storysound-app/app/Models/book_club_members.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class book_club_members extends Model
+class Book_club_members extends Model
 {
     use HasFactory, HasUuids;
 
--- Backend/storysound-app/app/Models/book_transaction.php
+++ Backend/storysound-app/app/Models/book_transaction.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class book_transaction extends Model
+class Book_transaction extends Model
 {
     use HasFactory;
 
