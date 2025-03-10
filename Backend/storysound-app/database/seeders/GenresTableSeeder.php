bcab632140d7330dd64f84ba0597c98ae2ff42c9
Venojan Shanmugarajah <VenojanShanmugarajah@gmail.com>
2025-03-10 12:01:53 +0530

    Genres seeder

---
+++ Backend/storysound-app/database/seeders/GenresTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GenresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Seeding Genres Table
+        DB::table('genres')->insert([
+            ['genre_id' => Str::uuid(), 'name' => 'Science Fiction', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['genre_id' => Str::uuid(), 'name' => 'Fantasy', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['genre_id' => Str::uuid(), 'name' => 'Mystery', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['genre_id' => Str::uuid(), 'name' => 'Romance', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['genre_id' => Str::uuid(), 'name' => 'Non-Fiction', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
+    }
+}
