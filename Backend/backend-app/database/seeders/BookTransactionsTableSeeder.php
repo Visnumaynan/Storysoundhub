f2af9fa476e2ef0e746c9eb5d2d9f3e2e598d2db
Visnumaynan <vvisnumaynan@gmail.com>
2025-03-20 20:00:25 +0530

    booktable seeder

---
+++ Backend/backend-app/database/seeders/BookTransactionsTableSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class BookTransactionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('book_transactions')->insert([
+            [
+                'transaction_id' => 1,
+                'book_id' => DB::table('books')->value('book_id'),
+                'buyer_id' => 1, // Buyer is different
+                'seller_id' =>2,
+                'price' => 19.99,
+                'status' => 'completed',
+                'transaction_date' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'transaction_id' => 2,
+                'book_id' => DB::table('books')->value('book_id'),
+                'buyer_id' => 1,
+                'seller_id' => 2,
+                'price' => 15.50,
+                'status' => 'pending',
+                'transaction_date' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'transaction_id' => 3,
+                'book_id' => DB::table('books')->value('book_id'),
+                'buyer_id' => 1,
+                'seller_id' => 2,
+                'price' => 12.75,
+                'status' => 'completed',
+                'transaction_date' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'transaction_id' => 4,
+                'book_id' => DB::table('books')->value('book_id'),
+                'buyer_id' =>1,
+                'seller_id' => 2,
+                'price' => 22.00,
+                'status' => 'pending',
+                'transaction_date' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'transaction_id' => 5,
+                'book_id' => DB::table('books')->value('book_id'),
+                'buyer_id' => 1,
+                'seller_id' =>2,
+                'price' => 18.90,
+                'status' => 'completed',
+                'transaction_date' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
