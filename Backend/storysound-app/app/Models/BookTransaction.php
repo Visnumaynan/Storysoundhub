d3ace40d36351c5a6a7cc3080aec5859b47e6906
Visnumaynan <vvisnumaynan@gmail.com>
2025-03-20 11:06:49 +0530

    book

--- Backend/storysound-app/app/Models/BookTransaction.php
+++ Backend/storysound-app/app/Models/BookTransaction.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BookTransaction extends Model
+class bookTransaction extends Model
 {
     use HasFactory;
 
