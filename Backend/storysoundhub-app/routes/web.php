200fcb5ad064936f46ac8d7d8e5033753308440b
Venojan Shanmugarajah <VenojanShanmugarajah@gmail.com>
2025-03-21 16:29:14 +0530

    test

--- Backend/storysoundhub-app/routes/web.php
+++ Backend/storysoundhub-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+Route::get('/checkout', [PaymentController::class, 'checkout']);
+Route::post('/charge', [PaymentController::class, 'charge'])->name('charge');
+
