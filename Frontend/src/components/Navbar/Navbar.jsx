873f23bd5596f70efd3b1fefe7d98fb0aa537b6a
Visnumaynan <vvisnumaynan@gmail.com>
2025-03-21 13:47:34 +0530

    navbar

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
             <li><Link to="/" className="nav-item">Home</Link></li>
             <li><Link to="/shop" className="nav-item">Shop</Link></li>
             <li><a href="#community" className="nav-item">Community</a></li>
-
+            <li>
               <Link to="/CartPage" className="cart-icon">
                 <FaHeart className="inline-block text-white" /> My List
               </Link>
